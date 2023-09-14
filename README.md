61d7efadbcc6c6fef0f08fa30d83376c2b0f5df2
Anurag Kinwar <anurag122002@gmail.com>
2023-09-14 14:26:38 +0530

    uploaded scraper and run code along with the csv file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DS_Salary_Project
+# DS_Salary_Project
--- data_collection.py
+++ data_collection.py
@@ -5,10 +5,12 @@ Created on Mon Sep 11 14:04:14 2023
 @author: kinwa
 """
 
-import glassdoor_scaper as gs
+import glassdoor_scraper as gs
 import pandas as pd
 
 path = r"chromedriver.exe"
 
 df = gs.get_jobs('data_scientist', 1000, False, path, 20)
 
+
+df.to_csv('glassdoor_jobs.csv', index=False)
